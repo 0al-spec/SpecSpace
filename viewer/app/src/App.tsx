e2a7334ab1d4d5446182cbb688662767108f9202
Egor Merkushev <gorkaedeep@gmail.com>
2026-06-28 14:54:01 +0300

    Revert "Route product workspaces to dedicated surface"
    This reverts commit 85b0b0d2bacba9a176d72e8757826a0025e3c730.

--- viewer/app/src/App.tsx
+++ viewer/app/src/App.tsx
@@ -5,34 +5,13 @@ import "./theme.css";
 import "./ConversationNode.css";
 import "./MessageNode.css";
 import "./SpecNode.css";
-import "./ProductWorkspacePage.css";
 import { ToastProvider } from "./Toast";
 import AppShell from "./AppShell";
-import ProductWorkspacePage from "./ProductWorkspacePage";
 import { useViewerAppController } from "./useViewerAppController";
 
-function productWorkspaceIdFromPath(pathname: string): string | null {
-  const trimmed = pathname.replace(/^\/+|\/+$/g, "");
-  if (!trimmed || trimmed === "index.html") {
-    return null;
-  }
-  if (trimmed.includes("/")) {
-    return null;
-  }
-  return decodeURIComponent(trimmed);
-}
-
-function LegacyAppShell() {
+function AppInner() {
   const controller = useViewerAppController();
   return <AppShell controller={controller} />;
-}
-
-function AppInner() {
-  const workspaceId = productWorkspaceIdFromPath(window.location.pathname);
-  if (workspaceId) {
-    return <ProductWorkspacePage workspaceId={workspaceId} />;
-  }
-  return <LegacyAppShell />;
 }
 
 export default function App() {
